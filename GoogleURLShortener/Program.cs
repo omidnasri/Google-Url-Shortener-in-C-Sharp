c60d0a1564f20de8b3335b67a75ffe79dce3783d
Omid Nasri <omid.nasri@outlook.com>
2015-12-17 21:10:04 +0330

    12-17-2015

---
+++ GoogleURLShortener/Program.cs
@@ -0,0 +1,37 @@
+﻿using Google.Apis.Services;
+using Google.Apis.Urlshortener.v1;
+using System;
+
+namespace GoogleURLShortener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+        }
+        public static string shortenIt(string url)
+        {
+            UrlshortenerService service = new UrlshortenerService(new BaseClientService.Initializer()
+            {
+                ApiKey = "API KEY from Google developer console",
+                ApplicationName = "Daimto URL shortener Sample",
+            });
+
+            var m = new Google.Apis.Urlshortener.v1.Data.Url();
+            m.LongUrl = url;
+            return service.Url.Insert(m).Execute().Id;
+        }
+
+        public static string unShortenIt(string url)
+        {
+            UrlshortenerService service = new UrlshortenerService(new BaseClientService.Initializer()
+            {
+                ApiKey = "API KEY from Google developer console",
+                ApplicationName = "Daimto URL shortener Sample",
+            });
+            return service.Url.Get(url).Execute().LongUrl;
+        }
+    }
+}
